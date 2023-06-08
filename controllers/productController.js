2b4908edae58a3f39a24cd06cee8f8f4349d4db8
Javabek <javabek1203@gmail.com>
2023-06-09 03:22:45 +0900

    feat: yangi productlarni restaurant user lar hosil qilishi

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,32 +1,46 @@
-let productController = module.exports;
+const assert = require("assert")
+const Definer = require("../lib/mistake")
+const Product = require("../models/Product")
+
+let productController = module.exports
 
 productController.getAllProducts = async (req, res) => {
   try {
-    console.log("Get: cont/getAllProducts");
+    console.log("Get: cont/getAllProducts")
   } catch (error) {
-    console.log(`Error, cont/getAllProducts, ${error.message}`);
-    res.json({ state: "fail", message: error.message });
+    console.log(`Error, cont/getAllProducts, ${error.message}`)
+    res.json({ state: "fail", message: error.message })
   }
-};
+}
 
 productController.addNewProduct = async (req, res) => {
   try {
-    console.log("POST: cont/addNewProduct");
+    console.log("POST: cont/addNewProduct")
+    assert(req.files, Definer.general_err3)
 
-    //TODO: product creation develop
+    const product = new Product()
+    let data = req.body
 
-    res.send("ok");
+    data.product_images = req.files.map((ele) => {
+      return ele.path;
+    })
+
+    const result = await product.addNewProductData(data, req.member);
+    const html = `<script> 
+                       alert(new thing added succesfully);
+                       window.location.replace('resto/products/menu');
+                  </script>`;
+    res.end(html);
   } catch (error) {
     console.log(`Error, cont/addNewProduct, ${error.message}`);
-    res.json({ state: "fail", message: error.message });
   }
-};
+}
 
 productController.updateChosenProduct = async (req, res) => {
   try {
-    console.log("POST: cont/updateChosenProduct");
+    console.log("POST: cont/updateChosenProduct")
   } catch (error) {
-    console.log(`Error, cont/updateChosenProduct, ${error.message}`);
-    res.json({ state: "fail", message: error.message });
+    console.log(`Error, cont/updateChosenProduct, ${error.message}`)
+    res.json({ state: "fail", message: error.message })
   }
-};
+}
--- utils/upload-multer.js
+++ utils/upload-multer.js
@@ -8,7 +8,6 @@ function getTargetImageStorage(address){
           cb(null, `./uploads/${address}`);
         },
         filename: function (req, file, cb) {
-          console.log(file);
           const extension = path.parse(file.originalname).ext;
           const random_name = uuid.v4()+ extension;
           cb(null, random_name);
@@ -22,6 +21,8 @@ return multer({storage: storage})
 }
 
 module.exports = makeUploader;
+
+
 // const product_storage = multer.diskStorage({
 //   destination: function (req, file, cb) {
 //     cb(null, "./uploads/products");
